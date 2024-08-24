d763e9f481d97e35eb754c184b98d175d343f35d
mohosin2126 <mohosin2126@gmail.com>
2024-08-24 23:01:12 +0600

    project init

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors=require('cors')
+
+// middleware
+app.use(express.json())
 
 
 
 
 
+// testing api 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
