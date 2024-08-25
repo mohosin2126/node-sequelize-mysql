7aff292443d87b5840604382e1d7cacc5eacb4e7
mohosin2126 <mohosin2126@gmail.com>
2024-08-25 21:05:34 +0600

    connected the database

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const cors=require('cors')
+'use strict';
 
-const apiRoutes=require("./src/routes/index")
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
 
-app.use("/api",apiRoutes)
+// Middleware
+app.use(cors());
+app.use(express.json());
 
+// Imports the route here
+const apiRoutes = require('./src/routes/index');
+app.use('/api', apiRoutes);
 
-// testing api 
+// Testing API
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Database Import and Sync
+const db = require('./src/database/models/index');
+
+// Sync database and start server
+db.sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
